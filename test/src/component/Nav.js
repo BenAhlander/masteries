23cd75088900e3e74e73bd05c3c46f0c98b4dfc1
Ben Ahlander <ben.ahlander@gmail.com>
2017-10-30 14:51:52 -0600

    fixing things

--- test/src/component/Nav.js
+++ test/src/component/Nav.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import '../css/nav.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-class Nav extends React.Component {
-  render() {
-    console.log(this.props)
+const Nav = function(props){
     return (
       <nav id="Nav">
-        <h4>{this.props.Prop}</h4>
+        <h4>{props.Prop}</h4>
           <Link to="/products"><button>Products</button></Link>
           <button>Add Product</button>
           <Link to='/theater'><button>Theater</button></Link>
       </nav>
     )
   }
-}
+
 export default Nav
